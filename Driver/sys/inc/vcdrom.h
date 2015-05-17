c23e6079af867f0fb93a5b94b2745c711d7549e6
VladSolovtsov350501 <cuxat1@mail.ru>
2015-05-18 01:06:49 +0300

    Добавлены *.c, *.h, makefile и sources для драйвера

---
+++ Driver/sys/inc/vcdrom.h
@@ -0,0 +1,36 @@
+#ifndef _FILE_DISK_
+#define _FILE_DISK_
+
+#define FILE_DISK_POOL_TAG 'ksiD'
+
+#ifndef __T
+#ifdef _NTDDK_
+#define __T(x)  L ## x
+#else
+#define __T(x)  x
+#endif
+#endif
+
+#ifndef _T
+#define _T(x)   __T(x)
+#endif
+
+#define DEVICE_BASE_NAME    _T("\\vcdrom")
+#define DEVICE_DIR_NAME     _T("\\Device")      DEVICE_BASE_NAME
+#define DEVICE_NAME_PREFIX  DEVICE_DIR_NAME     DEVICE_BASE_NAME
+
+#define FILE_DEVICE_FILE_DISK       0x8000
+
+#define IOCTL_FILE_DISK_OPEN_FILE   CTL_CODE(FILE_DEVICE_FILE_DISK, 0x800, METHOD_BUFFERED, FILE_READ_ACCESS | FILE_WRITE_ACCESS)
+#define IOCTL_FILE_DISK_CLOSE_FILE  CTL_CODE(FILE_DEVICE_FILE_DISK, 0x801, METHOD_BUFFERED, FILE_READ_ACCESS | FILE_WRITE_ACCESS)
+#define IOCTL_FILE_DISK_QUERY_FILE  CTL_CODE(FILE_DEVICE_FILE_DISK, 0x802, METHOD_BUFFERED, FILE_READ_ACCESS)
+
+typedef struct _OPEN_FILE_INFORMATION {
+    LARGE_INTEGER   FileSize;
+    BOOLEAN         ReadOnly;
+    UCHAR           DriveLetter;
+    USHORT          FileNameLength;
+    UCHAR           FileName[1];
+} OPEN_FILE_INFORMATION, *POPEN_FILE_INFORMATION;
+
+#endif
